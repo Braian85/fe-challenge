43d9c569d808e581cd6c1d19138907e27f9a6927
Braian85 <braian@gmail.com>
2021-08-30 11:05:42 -0300

    Code clean up.

--- src/App.js
+++ src/App.js
@@ -9,64 +9,44 @@ function App() {
     posts: []
 
   })
-
   const [comments, setComments] = useState(
     {
       dataReady: false,
       comments: []
-
     }
-
   );
 
   // Hook para cargar Users. 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts")
     .then( res => {
-        //console.log(res)
-        
-        setPosts({posts: res.data, dataReady: true})
-        //console.log("POSTS")
-        
+       setPosts({posts: res.data, dataReady: true})
+      
     })
     .catch(err => {
       console.log(err)
     })
-  }, [/* posts.dataReady */])
+  }, [])
 
-  //Hook para cargar Comments.
+  //Hook to load Comments.
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/comments")
     .then( res => {
-        //console.log(res)
-        
-        setComments({comments: res.data, dataReady: true})
-      /*   console.log("Trajo Comments.") */
-        
+               setComments({comments: res.data, dataReady: true})
+             
     })
     .catch(err => {
       console.log(err)
     })
-  }, [/* comments.dataReady */]) 
+  }, []) 
 
   return (
-
-    
-    <div className="App">
-      
-    {console.log("Render")}
-
-{/* <div>{console.log("Comments:", comments)}</div> */}
-     
-    {/*  {console.log("FAFAFA:", comments.comments.filter(f =>f.postId == 1))} */}
-
+      <div className="App">
       <nav className="nav"><span><strong className="str">COMPANY </strong>SOCIAL NETWORK</span></nav>
       {posts.dataReady && comments.dataReady  ? posts.posts.map((data, i) =>
        (<Users key={i} clave={i} data={data} com= {comments} />)) 
        :(<h1>Loading ...{console.log("Cargando...")}</h1>)}
- 
-    </div>
+     </div>
   );
 }
-
 export default App;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,33 +3,30 @@ import "./Users.css";
 
   function Users(props) {
 
+   function handleClick(e) {
+        const id = e.target.id;
+        console.log(id);
+      }
    
       return (
        <div className="card">
-           
-         
            <div className="header">
            <img className="img" src={`https://i.pravatar.cc/150?img=${props.data.userId}`} alt="User" ></img>
            <div className="title">{props.data.title} </div> 
            </div>
-             
            <div className="body">{props.data.body}  </div> 
            <div className="comment-header">
-               <button className="btn">Comments</button></div>
+               <button id={props.data.id} className="btn" onClick={ handleClick}>Comments</button></div>
             {props.com.comments.filter(f => f.postId=== props.clave+1).map(e => (
-
-                <div className="comments">
-                
+               <div className="comments">
                <div className="contact-info">
-                    <div className="name">  {e.name}</div>
-                    <div className="email"> {e.email}</div>
+               <div className="name">  {e.name}</div>
+               <div className="email"> {e.email}</div>
                </div>
-                    <div className="bodycom"><span> {e.body}</span></div>
+               <div className="bodycom"><span> {e.body}</span></div>
            </div>
-
             ))}
-       
-       </div>
+          </div>
     )
 }
 
